2a56436f07d569793286b1ffb1a70c7bd74a53c3
Daniele Megna <megna.dany@gmail.com>
2015-09-01 12:39:41 +0200

    Implemented has_same_seed card method

--- lib/card.rb
+++ lib/card.rb
@@ -3,9 +3,13 @@ class Card
   attr_reader :seed
   attr_reader :number
 
-  def initialize(number_and_seed)
+  def initialize number_and_seed
     @number = number_and_seed.slice(0, 1).to_i
-    @seed   = number_and_seed.slice(1, 1)
+    @seed   = number_and_seed.slice(1)
+  end
+
+  def has_same_seed? other_card
+    @seed == other_card.seed
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,9 +3,19 @@ require 'card'
 RSpec.describe Card do
 
   it "inits itself with number and seed" do
-    card = Card.new("4D")
+    card = Card.new "4D"
     expect(card.number).to eq 4
     expect(card.seed).to eq "D"
+  end
+
+  it "recognize cards of the same seed" do
+    three_of_coppe  = Card.new "3C"
+    eight_of_coppe  = Card.new "8C"
+    four_of_spade   = Card.new "4S"
+
+    three_of_coppe.has_same_seed?(eight_of_coppe).should be_true
+    three_of_coppe.has_same_seed?(four_of_spade).should be_false
+    four_of_spade.has_same_seed?(eight_of_coppe).should be_false
   end
 
 end
