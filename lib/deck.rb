0b8d0ee8749dd23ba078cc246f414cbae556abbe
Daniele Megna <megna.dany@gmail.com>
2015-09-06 19:44:57 +0200

    Developing deck class

---
+++ lib/deck.rb
@@ -0,0 +1,15 @@
+class Deck
+
+  def initialize cards
+    
+  end
+  
+  def size
+    40
+  end
+
+  def get_current_valuable_card
+    Card.new "5C" 
+  end
+
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,20 @@
+require 'deck'
+
+RSpec.describe Deck do
+
+  before(:each) do
+    @deck = Deck.new "9S 5C 6C 10B 6B 6D 8C 10C 2S 1S 6S 5D " + 
+      "9D 10S 2D 4B 4C 4D 7D 2C 1C 8B 1B 5S 3D " + 
+      "2B 8S 3B 7B 7C 3S 8D 4S 7S 1D 10D 9C 9B 5B 3C"
+  end
+
+  it "init with cards identifiers and get size" do
+    expect(@deck.size).to eq 40
+  end
+
+  it "returns the current valuable card" do
+    expected = Card.new "5C"
+    expect(@deck.get_current_valuable_card).to eq expected
+  end
+
+end
