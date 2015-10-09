8c41b877a0a00fb999b6d52b6a3101b36979be20
Daniele Megna <daniele.megna@xpeppers.com>
2015-10-09 16:08:19 +0200

    Added filps_count to verdict

--- lib/droidealer.rb
+++ lib/droidealer.rb
@@ -3,7 +3,7 @@ require 'verdict'
 class Droidealer
 
   def generate_game_verdict deck_string
-    Verdict.new false, deck_string
+    Verdict.new false, 20, deck_string
   end
 
 end
--- lib/verdict.rb
+++ lib/verdict.rb
@@ -1,9 +1,11 @@
 class Verdict
 
   attr_reader :deck_snapshot
+  attr_reader :filps_count
   
-  def initialize is_victory, deck_snapshot
+  def initialize is_victory, filps_count, deck_snapshot
     @is_victory = is_victory
+    @filps_count = filps_count
     @deck_snapshot = deck_snapshot
   end
 
