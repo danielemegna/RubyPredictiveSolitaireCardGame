b4b0f0fc7a48de65a4650c149c943619f9bfb483
Daniele Megna <daniele.megna@xpeppers.com>
2015-10-09 18:14:46 +0200

    Added next acceptance test for droidealer'

--- spec/droidealer_spec.rb
+++ spec/droidealer_spec.rb
@@ -38,4 +38,20 @@ RSpec.describe Droidealer do
     expect(verdict.deck_snapshot).to eq expected_snapshot
   end
 
+  xit 'win with trivial victory' do
+    @droid = Droidealer.new
+
+    deck_string = "2C 1C 2B 1B 2D 1D 2S 1S 4C 3C 4B 3B " + 
+      "4D 3D 4S 3S 6C 5C 6B 5B 6D 5D 6S 5S 8C " + 
+      "7C 8B 7B 8D 7D 8S 7S 10C 9C 10B 9B 10D 9D 10S 9S"
+      
+    verdict = @droid.generate_game_verdict(deck_string)
+
+    expect(verdict.victory?).to be true
+    expect(verdict.cards_count).to eq 0
+    expect(verdict.rounds_count).to eq 0
+    expect(verdict.flips_count).to eq 0
+    expect(verdict.deck_snapshot).to eq ""
+  end
+
 end
