74fa98ba42e7f45bdbe3d4e134a8325198ba14b8
Daniele Megna <megna.dany@gmail.com>
2015-10-31 15:01:58 +0100

    Added verdict to string tests with filp and removes

--- spec/verdict_spec.rb
+++ spec/verdict_spec.rb
@@ -18,4 +18,41 @@ RSpec.describe Droidealer do
     expect(verdict.to_s).to eq expected
   end
 
+  it 'prints himself after flips' do
+    deck = Deck.new "9S 5C 6C 10B 6B 6D 8C 10C"
+    9.times{ deck.flip_cards }
+
+    verdict = Verdict.new deck
+
+    expected =
+      "Verdict\n" +
+      "-----------\n" +
+      "- Victory?\t\tNo\n" +
+      "- Flips count:\t\t9\n" +
+      "- Rounds count:\t\t2\n" +
+      "- Cards count:\t\t8\n" +
+      "- Deck Snapshot:\t9S 5C 6C 10B 6B 6D 8C 10C"
+
+    expect(verdict.to_s).to eq expected
+  end
+
+  it 'prints himself after remove' do
+    deck = Deck.new "9S 5C 6C 10B 6B 6D 8C 10C"
+    deck.flip_cards
+    4.times{ deck.remove_current_valuable_card }
+
+    verdict = Verdict.new deck
+
+    expected =
+      "Verdict\n" +
+      "-----------\n" +
+      "- Victory?\t\tNo\n" +
+      "- Flips count:\t\t3\n" +
+      "- Rounds count:\t\t0\n" +
+      "- Cards count:\t\t4\n" +
+      "- Deck Snapshot:\t6B 6D 8C 10C"
+
+    expect(verdict.to_s).to eq expected
+  end
+
 end
