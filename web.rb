c4dcbd048ba2250d9469a028df560ad3cb2ca086
Daniele Megna <megna.dany@gmail.com>
2015-12-08 18:56:40 +0100

    Styles on web view

--- web.rb
+++ web.rb
@@ -8,7 +8,7 @@ set :bind, '0.0.0.0'
 
 get '/' do
   
-  body = '<pre style="padding:40px; font-size:20px">'
+  body = '<pre style="width:800px;white-space:pre-wrap;padding:20px;font-size:35px">'
   body += Droidealer.new.random_stats_as_string
   body += '</pre>'
 
